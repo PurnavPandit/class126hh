d506e538589696c56d82dbe4274695f4a376b67e
PurnavPandit <77737419+PurnavPandit@users.noreply.github.com>
2021-08-17 12:40:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
 song1 = "";
 song2 = "";
+scoreRight = 0;
+scoreLeft = 0;
+rightWristX = false;
+rightWristY = false;
+leftWristX = false;
+leftWristY = false;
 function preload(){
     song1 = loadSound("believer Mp3 Imagine dragon.mp3");
-    song2 = loadSound("Thunder.m4a");
+    song2 = loadSound("Thunder.mp3");
 }
 function setup(){
-    canvas = createCanvas(600,500);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.hide();
+    canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video,0,0,600,500); 
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+
+  function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreRight =  results[0].pose.keypoints[10].score;
+	scoreLeft =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x.true;
+	rightWristY = results[0].pose.rightWrist.y.true;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+    }
 }
